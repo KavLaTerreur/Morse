be2bbbae30424a1949b9d6b6ec212b8cb5de1384
WheelyTM <pepin.william@carrefour.cegepvicto.ca>
2019-03-27 13:58:11 -0400

    Ajout de la lettre T

---
+++ t.py
@@ -0,0 +1,16 @@
+
+from gpiozero import LED
+from time import sleep
+
+
+point = 0.2
+tiret = point * 3
+espace = point
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(tiret)
+    led.off()
+
