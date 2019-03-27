f04c639d095fd59f1ada1ac6c8151223d90b035a
WheelyTM <pepin.william@carrefour.cegepvicto.ca>
2019-03-27 13:57:49 -0400

    Ajout de la lettre s

---
+++ s.py
@@ -0,0 +1,23 @@
+
+from gpiozero import LED
+from time import sleep
+
+
+point = 0.2
+tiret = point * 3
+espace = point
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(point)
+    led.off()
+    sleep(espace)
+    led.on()
+    sleep(point)
+    led.off()
+    sleep(espace)
+    led.on()
+    sleep(point)
+
