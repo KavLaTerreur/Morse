bd637cf052e8f9bab9ab05fa16cbe7f81532ce0a
kev <48568317+KavLaTerreur@users.noreply.github.com>
2019-03-28 15:31:03 -0400

    Update c.py

--- c.py
+++ c.py
@@ -7,7 +7,7 @@ espace = point
 
 led = LED(17)
 
-def C(
+def C()
     led.on()
     sleep(tiret)
     led.off()
@@ -24,7 +24,7 @@ def C(
     sleep(point)
     led.off()
     sleep(espace)
-)
+
 
 
 
