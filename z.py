ceda4eaa524c906a11db559da8688d92e68f4d0f
kev <48568317+KavLaTerreur@users.noreply.github.com>
2019-03-28 16:14:33 -0400

    Update z.py

--- z.py
+++ z.py
@@ -1,13 +1,14 @@
 from gpiozero import LED
 from time import sleep
 
-point = 0.2
-tiret = point * 3
-espace = point
 
-led = LED(17)
 
 def Z():
+    point = 0.2
+    tiret = point * 3
+    espace = point
+
+    led = LED(17)
     led.on()
     sleep(tiret)
     led.off()
