7cd1fd87513613cf82181fbf06eb34a937ec9aaf
kev <48568317+KavLaTerreur@users.noreply.github.com>
2019-03-28 16:13:33 -0400

    Update w.py

--- w.py
+++ w.py
@@ -2,13 +2,14 @@ from gpiozero import LED
 from time import sleep
 
 
-point = 0.2
-tiret = point * 3
-espace = point
 
-led = LED(17)
 
 def W():
+    point = 0.2
+    tiret = point * 3
+    espace = point
+
+    led = LED(17)
     led.on()
     sleep(point)
     led.off()
