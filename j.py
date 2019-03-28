2f07ab9a01b32a78c95b555aa548f011874bb120
kev <48568317+KavLaTerreur@users.noreply.github.com>
2019-03-28 15:34:09 -0400

    Update j.py

--- j.py
+++ j.py
@@ -7,7 +7,7 @@ espace = point
 
 led = LED(17)
 
-def J(
+def J():
     led.on()
     sleep(point)
     led.off()
@@ -24,7 +24,7 @@ def J(
     sleep(tiret)
     led.off()
     sleep(espace)
-)	
+
 
 
 
