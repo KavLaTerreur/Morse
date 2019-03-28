262f465fa5e3bf9061a865ce3141dd9aa81e953b
kev <48568317+KavLaTerreur@users.noreply.github.com>
2019-03-28 16:11:23 -0400

    Update o.py

--- o.py
+++ o.py
@@ -1,13 +1,14 @@
 from gpiozero import LED
 from time import sleep
 
-point = 0.2
-tiret = point * 3
-espace = point
 
-led = LED(17)
 
 def O():
+    point = 0.2
+    tiret = point * 3
+    espace = point
+
+    led = LED(17)
     led.on()
     sleep(tiret)
     led.off()
