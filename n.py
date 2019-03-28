4d61cdcd244460b56edf51d19f145b6d73d83f8e
kev <48568317+KavLaTerreur@users.noreply.github.com>
2019-03-28 16:11:09 -0400

    Update n.py

--- n.py
+++ n.py
@@ -1,13 +1,13 @@
 from gpiozero import LED
 from time import sleep
 
-point = 0.2
-tiret = point * 3
-espace = point
-
-led = LED(17)
 
 def N():
+    point = 0.2
+    tiret = point * 3
+    espace = point
+
+    led = LED(17)
     led.on()
     sleep(tiret)
     led.off()
